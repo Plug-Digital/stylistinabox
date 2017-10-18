1c5c65d1fbc1ad4445cb7683b706b0199bcda190
Zhang <aaron@plugdetroit.com>
2017-10-18 15:47:13 -0400

    version update

--- wp-content/themes/enfold/functions.php
+++ wp-content/themes/enfold/functions.php
@@ -298,7 +298,7 @@ if(!function_exists('avia_register_frontend_scripts'))
 
 	function avia_register_frontend_scripts()
 	{
-		$vn = "4.1";
+		$vn = "4.2";
 		
 		$template_url 		= get_template_directory_uri();
 		$child_theme_url 	= get_stylesheet_directory_uri();
