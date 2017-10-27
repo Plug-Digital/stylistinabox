b0b2eea8d9b618f290486136c6e6e7588e7e0de1
Zhang <aaron@plugdetroit.com>
2017-10-27 18:36:35 -0400

    update selecting style profile

--- wp-content/themes/enfold/js/custom.js
+++ wp-content/themes/enfold/js/custom.js
@@ -1,9 +1,18 @@
 (function($)
 {	
     $(document).ready(function() {
-    	$('.select-profile').on('click', function() {
+    	$('.select-profile').on('click', function(e) {
     		var selection = $(this).attr('data-select');
-    		$('#get-started input[name="style_profile[]"').filter('input[value=' + selection + ']').parent().trigger('click');
+    		// $('#get-started input[name="style_profile[]"').filter('input[value=' + selection + ']').parent().trigger('click');
+
+    		$('#get-started input[name="style_profile[]"]:checked').prop('checked', false);
+    		$('#get-started input[name="style_profile[]"]').filter('input[value=' + selection + ']').prop('checked', true);
+    		$('#get-started input[name="style_profile[]"]').filter('input[value=' + selection + ']').parent().siblings().removeClass('active');
+    		$('#get-started input[name="style_profile[]"]').filter('input[value=' + selection + ']').parent().siblings().find('.um-field-radio-state i').removeClass('um-icon-android-radio-button-on').addClass('um-icon-android-radio-button-off');
+
+    		$('#get-started input[name="style_profile[]"]').filter('input[value=' + selection + ']').parent().addClass('active');
+    		$('#get-started input[name="style_profile[]"]').filter('input[value=' + selection + ']').parent().find('.um-field-radio-state i').removeClass('um-icon-android-radio-button-off').addClass('um-icon-android-radio-button-on');
+
     	})
     });
 })(jQuery);
