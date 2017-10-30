e209a8a5c7e21ab6145010cbe1f20d6fdbd9917e
Zhang <aaron@plugdetroit.com>
2017-10-30 04:58:25 -0400

    update header navigation

--- wp-content/themes/enfold/js/custom.js
+++ wp-content/themes/enfold/js/custom.js
@@ -13,6 +13,21 @@
     		$('#get-started input[name="style_profile[]"]').filter('input[value=' + selection + ']').parent().addClass('active');
     		$('#get-started input[name="style_profile[]"]').filter('input[value=' + selection + ']').parent().find('.um-field-radio-state i').removeClass('um-icon-android-radio-button-off').addClass('um-icon-android-radio-button-on');
 
-    	})
+    	});
+
+    	$('#avia-menu .menu-item a').on('click', function(e) {
+    		var selection = $(this).find('.avia-menu-text').text();
+    		selection = selection.indexOf('Women') >= 0 ? 'Women' : 'Men';
+    		// $('#get-started input[name="style_profile[]"').filter('input[value=' + selection + ']').parent().trigger('click');
+
+    		$('#get-started input[name="style_profile[]"]:checked').prop('checked', false);
+    		$('#get-started input[name="style_profile[]"]').filter('input[value=' + selection + ']').prop('checked', true);
+    		$('#get-started input[name="style_profile[]"]').filter('input[value=' + selection + ']').parent().siblings().removeClass('active');
+    		$('#get-started input[name="style_profile[]"]').filter('input[value=' + selection + ']').parent().siblings().find('.um-field-radio-state i').removeClass('um-icon-android-radio-button-on').addClass('um-icon-android-radio-button-off');
+
+    		$('#get-started input[name="style_profile[]"]').filter('input[value=' + selection + ']').parent().addClass('active');
+    		$('#get-started input[name="style_profile[]"]').filter('input[value=' + selection + ']').parent().find('.um-field-radio-state i').removeClass('um-icon-android-radio-button-off').addClass('um-icon-android-radio-button-on');
+
+    	});
     });
 })(jQuery);
