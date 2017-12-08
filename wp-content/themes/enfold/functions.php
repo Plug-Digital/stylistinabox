436632622021785b1733704d4cb3c8e1b565cbe5
Zhang <dreammate0621@gmail.com>
2017-12-08 23:22:51 +0800

    update card val

--- wp-content/themes/enfold/functions.php
+++ wp-content/themes/enfold/functions.php
@@ -312,8 +312,18 @@ if(!function_exists('avia_register_frontend_scripts'))
 		wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'wp-mediaelement' );
 
-		wp_enqueue_script( 'avia-custom',  $template_url.'/js/custom.js');
-
+		wp_register_script( 'avia-custom',  $template_url.'/js/custom.js');
+		if(is_user_logged_in()) {
+			global $ultimatemember;
+			
+	    	$userInfo = array(
+				'first_name' => um_user('first_name'),
+				'last_name' => um_user('last_name')
+			);
+			wp_localize_script( 'avia-custom', 'user_info', $userInfo );
+	    }
+	    wp_enqueue_script( 'avia-custom' );
+		
 
 		if ( is_singular() && get_option( 'thread_comments' ) ) { wp_enqueue_script( 'comment-reply' ); }
 
--- wp-content/themes/enfold/js/custom.js
+++ wp-content/themes/enfold/js/custom.js
@@ -29,5 +29,52 @@
     		$('#get-started input[name="style_profile[]"]').filter('input[value=' + selection + ']').parent().find('.um-field-radio-state i').removeClass('um-icon-android-radio-button-off').addClass('um-icon-android-radio-button-on');
 
     	});
+
+        $('.fullstripe-form-input[data-stripe="number"]').keyup(function () {
+            var v = $(this).val().replace(/\s+/g, '').replace(/[^0-9]/gi, '');
+            var matches = v.match(/\d{1,16}/g);
+            var match = matches && matches[0] || '';
+            var cardnum = "";
+            if (match.length > 1 && (match.slice(0, 2) == "34" || match.slice(0, 2) == "37")) {
+                //amex
+                if (match.length > 15) {
+                    match = match.slice(0, 15);
+                }
+                for (i = 0, len = match.length; i < len; i++) {
+                    if (i == 4 || i == 10) {
+                        cardnum += " ";
+                    }
+                    cardnum += match[i];
+                }
+            } else {
+                var parts = [];
+                for (i = 0, len = match.length; i < len; i += 4) {
+                    parts.push(match.substring(i, i + 4));
+                }
+                if (parts.length) {
+                    cardnum = parts.join(' ');
+                } else {
+                    cardnum = match;
+                }
+            }
+            $(this).val(cardnum);
+        });
+
+        $('.fullstripe-form-input[data-stripe="exp-month"]').keyup(function () {
+            var v = $(this).val();
+            var matches = v.match(/\d{1,2}/g);
+            var expMon = matches && matches[0] || '';
+            if(parseInt(expMon) > 12)
+                expMon = 12;
+            $(this).val(expMon);
+        });
+
+        $('.fullstripe-form-input[data-stripe="exp-year"]').keyup(function () {
+            var v = $(this).val();
+            var matches = v.match(/\d{1,4}/g);
+            var exp-year = matches && matches[0] || '';
+            
+            $(this).val(expYear);
+        });
     });
 })(jQuery);
