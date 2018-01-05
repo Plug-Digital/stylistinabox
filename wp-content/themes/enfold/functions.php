c40a1fa3048348b862e8460cb7fa8143a0b2ac06
Zhang <dreammate0621@gmail.com>
2018-01-05 08:05:21 -0500

    update debug code

--- wp-content/themes/enfold/functions.php
+++ wp-content/themes/enfold/functions.php
@@ -710,9 +710,8 @@ function set_customer_price( $entry, $form ) {
     // set the arguments for the add_user_meta() function
     $meta_key = 'gform_customer_price';
 
-    $rangeArr = explode(' ­-­ ', $entry[38]);
-    print_r($rangeArr);
-    $meta_value = str_replace('$', '', $rangeArr[1]);
+	preg_match_all('/\$([0-9]+[\.]*[0-9]*)/', $entry[38], $match);
+	$meta_value = $match[1][1];
     print_r($meta_value);
     exit;
     
